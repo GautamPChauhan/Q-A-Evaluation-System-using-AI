2b1beb3b9795e2c7e9d332f3fecd4a8e5525163a
Gautam Chauhan <136146320+GautamPChauhan@users.noreply.github.com>
2026-04-01 11:05:22 +0530

    Delete edu_ai directory

--- edu_ai/keyword_engine/scorer.py
+++
@@ -1,8 +0,0 @@
-def calculate_marks(raw_score, keyword_config):
-    max_raw = sum(keyword_config["weights"].values())
-    max_marks = keyword_config["max_marks"]
-
-    if max_raw == 0:
-        return 0.0
-
-    return round((raw_score / max_raw) * max_marks, 2)
